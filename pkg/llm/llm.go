027661a460a65a1d7e8eeeaaa1ce7a4cf952e8b2
WinPooh32 <winpooh32@yandex.ru>
2025-01-03 18:44:00 +1000

    Initial upload

---
+++ pkg/llm/llm.go
@@ -0,0 +1,49 @@
+package llm
+
+type MessageGenerator interface {
+	// Generate generates the next message of the history.
+	Generate(history []Message, tools []ToolFunction) (Message, error)
+}
+
+type Message struct {
+	Role      Role
+	Content   string
+	ToolCalls []ToolCallFunction
+}
+
+type Role int
+
+const (
+	System Role = iota
+	User
+	Assistant
+	Tool
+)
+
+type ToolFunction struct {
+	Name        string
+	Description string
+	Parameters  map[string]PropertyType
+}
+
+type PropertyType int
+
+const (
+	String PropertyType = iota
+	Number
+	Integer
+	Array
+)
+
+type FunctionProperty struct {
+	Type          PropertyType
+	ArrayItemType PropertyType
+	Description   string
+	Enum          []string
+	Required      bool
+}
+
+type ToolCallFunction struct {
+	Name      string
+	Arguments map[string]any
+}
